01235514643062c454c1958b042036244c6ea72f
Romina Rebolledo <rominarebolledo@gmail.com>
2021-11-02 12:25:15 +0100

    modified test-digits.js

---
+++ problems/medium/test_digits.js
@@ -0,0 +1,16 @@
+import assert from "assert";
+import { digits } from "./digits";
+
+describe("digits", () => {
+  it("should return [2, 3, 4, 2] when the function has [2342] as argument ", () =>
+    assert.deepEqual(digits([2342]), [2, 3, 4, 2]));
+
+  it("should return [1, 2, 3, 4, 5, 6] when the function has [123456] as argument", () =>
+    assert.deepEqual(digits([123456]), [1, 2, 3, 4, 5, 6]));
+
+  it("should return [1] when the function has [1] as argument", () =>
+    assert.deepEqual(digits([1]), [1]));
+
+  it("should return [9, 8, 1, 0, 9, 2, 8, 3, 0, 9, 1, 2] when the function has [981092830912] as argument", () =>
+    assert.deepEqual(digits([981092830912]), [9, 8, 1, 0, 9, 2, 8, 3, 0, 9, 1, 2]));
+});
